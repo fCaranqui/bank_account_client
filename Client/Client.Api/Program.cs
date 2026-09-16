0dc8582c3e23c3c10a567d5de746bb50d2951e63
Fernando Caranqui <yaguar-2012@hotmail.com>
2026-09-15 20:41:12 -0500

    Add Serilog to Client.Api
    Console sink plus daily rolling file sink (logs/client-.log),
    FromLogContext enrichment, and UseSerilogRequestLogging for
    request-level logging. Mirrors Account.Api.

--- Client/Client.Api/Program.cs
+++ Client/Client.Api/Program.cs
@@ -1,4 +1,14 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    .WriteTo.File("logs/client-.log", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 
@@ -7,6 +17,8 @@ builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
