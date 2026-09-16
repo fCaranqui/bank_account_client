4bbd54b2f89ea9cbd47232e6a57c2b91dc6a79a0
Fernando Caranqui <yaguar-2012@hotmail.com>
2026-09-16 01:47:45 -0500

    test(account-domain): add unit tests for Cuenta/Movimiento
    RegisterMovement happy path, SaldoNoDisponibleException on an overdrawing
    Retiro, CuentaInactivaException on a deactivated cuenta, Activate/Deactivate.
    Exercised entirely through Cuenta's public API.

---
+++ Account/Account.Tests/Domain/CuentaTests.cs
@@ -0,0 +1,106 @@
+using Account.Domain;
+using Account.Domain.Exceptions;
+
+namespace Account.Tests.Domain;
+
+public class CuentaTests
+{
+    [Fact]
+    public void Constructor_ValidData_CreatesCuenta()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        Assert.Equal("001-001", cuenta.NumeroCuenta);
+        Assert.Equal(TipoCuenta.Ahorro, cuenta.TipoCuenta);
+        Assert.Equal(100m, cuenta.SaldoInicial);
+        Assert.Equal(100m, cuenta.SaldoDisponible);
+        Assert.True(cuenta.Estado);
+    }
+
+    [Fact]
+    public void Constructor_EmptyNumeroCuenta_Throws()
+    {
+        Assert.Throws<ArgumentException>(() => new Cuenta(string.Empty, TipoCuenta.Ahorro, 100m, "CLI-001"));
+    }
+
+    [Fact]
+    public void Constructor_NegativeSaldoInicial_Throws()
+    {
+        Assert.Throws<ArgumentOutOfRangeException>(() => new Cuenta("001-001", TipoCuenta.Ahorro, -1m, "CLI-001"));
+    }
+
+    [Fact]
+    public void Constructor_EmptyClienteId_Throws()
+    {
+        Assert.Throws<ArgumentException>(() => new Cuenta("001-001", TipoCuenta.Ahorro, 100m, string.Empty));
+    }
+
+    [Fact]
+    public void RegisterMovement_Deposito_IncreasesSaldoDisponible()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        var movimiento = cuenta.RegisterMovement(TipoMovimiento.Deposito, 50m);
+
+        Assert.Equal(150m, cuenta.SaldoDisponible);
+        Assert.Equal(50m, movimiento.Valor);
+        Assert.Equal(150m, movimiento.Saldo);
+    }
+
+    [Fact]
+    public void RegisterMovement_Retiro_DecreasesSaldoDisponible()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        var movimiento = cuenta.RegisterMovement(TipoMovimiento.Retiro, 40m);
+
+        Assert.Equal(60m, cuenta.SaldoDisponible);
+        Assert.Equal(-40m, movimiento.Valor);
+    }
+
+    [Fact]
+    public void RegisterMovement_RetiroMayorQueSaldo_ThrowsSaldoNoDisponibleException()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        Assert.Throws<SaldoNoDisponibleException>(() => cuenta.RegisterMovement(TipoMovimiento.Retiro, 150m));
+    }
+
+    [Fact]
+    public void RegisterMovement_CuentaDesactivada_ThrowsCuentaInactivaException()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+        cuenta.Deactivate();
+
+        Assert.Throws<CuentaInactivaException>(() => cuenta.RegisterMovement(TipoMovimiento.Deposito, 10m));
+    }
+
+    [Fact]
+    public void RegisterMovement_ValorCero_Throws()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        Assert.Throws<ArgumentException>(() => cuenta.RegisterMovement(TipoMovimiento.Deposito, 0m));
+    }
+
+    [Fact]
+    public void Deactivate_SetsEstadoFalse()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        cuenta.Deactivate();
+
+        Assert.False(cuenta.Estado);
+    }
+
+    [Fact]
+    public void Activate_SetsEstadoTrue()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+        cuenta.Deactivate();
+
+        cuenta.Activate();
+
+        Assert.True(cuenta.Estado);
+    }
+}
---
+++ Account/Account.Tests/Domain/MovimientoTests.cs
@@ -0,0 +1,51 @@
+using Account.Domain;
+
+namespace Account.Tests.Domain;
+
+public class MovimientoTests
+{
+    [Fact]
+    public void RegisterMovement_Deposito_CreatesMovimientoWithPositiveValor()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        var movimiento = cuenta.RegisterMovement(TipoMovimiento.Deposito, 25m);
+
+        Assert.Equal(TipoMovimiento.Deposito, movimiento.TipoMovimiento);
+        Assert.Equal(25m, movimiento.Valor);
+        Assert.Equal(cuenta.Id, movimiento.CuentaId);
+    }
+
+    [Fact]
+    public void RegisterMovement_Retiro_CreatesMovimientoWithNegativeValor()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        var movimiento = cuenta.RegisterMovement(TipoMovimiento.Retiro, 25m);
+
+        Assert.Equal(TipoMovimiento.Retiro, movimiento.TipoMovimiento);
+        Assert.Equal(-25m, movimiento.Valor);
+    }
+
+    [Fact]
+    public void RegisterMovement_AddsToCuentaMovimientosCollection()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        cuenta.RegisterMovement(TipoMovimiento.Deposito, 10m);
+        cuenta.RegisterMovement(TipoMovimiento.Deposito, 20m);
+
+        Assert.Equal(2, cuenta.Movimientos.Count);
+    }
+
+    [Fact]
+    public void RegisterMovement_SaldoReflectsRunningBalance()
+    {
+        var cuenta = new Cuenta("001-001", TipoCuenta.Ahorro, 100m, "CLI-001");
+
+        cuenta.RegisterMovement(TipoMovimiento.Deposito, 50m);
+        var segundo = cuenta.RegisterMovement(TipoMovimiento.Retiro, 30m);
+
+        Assert.Equal(120m, segundo.Saldo);
+    }
+}
